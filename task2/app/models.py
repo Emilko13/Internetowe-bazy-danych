8c510ca290cacc71a234b37a2e0a13d550eafed4
Emilko13 <emil.kowalczyk@gmail.com>
2026-01-12 20:25:51 +0100

    delete old files

--- task2/app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
-
-class User(Base):
-    __tablename__ = 'sqlalchemy_users'
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), index=True)
-    email = Column(String(255), unique=True, index=True)
